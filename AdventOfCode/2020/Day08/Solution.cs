20a94503e5df42ee9af298e805a1ed261a31d67d
Martijn <karten702@gmail.com>
2020-12-08 08:27:39 +0100

    2020d8p1

--- AdventOfCode/2020/Day08/Solution.cs
+++ AdventOfCode/2020/Day08/Solution.cs
@@ -19,9 +19,39 @@ namespace AdventOfCode.Y2020.Day08 {
             Console.WriteLine("Part 2:");
             Console.WriteLine(Part2());
         }
-        public static bool Part1()
+        public static int Part1()
         {
-            return true;
+            int acc = 0;
+            HashSet<int> codes = new HashSet<int>();
+
+            for (int i = 0; i < Input.Length; i++)
+            {
+                if (!codes.Contains(i))
+                {
+                    codes.Add(i);
+                    var instruction = Input[i].Split(' ');
+                    switch (instruction[0])
+                    {
+                        case "acc":
+                            acc = instruction[1][0] switch
+                            {
+                                '+' => acc + Convert.ToInt32(instruction[1].Substring(1)),
+                                '-' => acc - Convert.ToInt32(instruction[1].Substring(1))
+                            };
+                            break;
+                        case "jmp":
+                            i = instruction[1][0] switch
+                            {
+                                '+' => i + Convert.ToInt32(instruction[1].Substring(1)) - 1,
+                                '-' => i - Convert.ToInt32(instruction[1].Substring(1)) - 1
+                            };
+                            break;
+                    }
+                }
+                else
+                    break;
+            }
+            return acc;
         }
 
         public static bool Part2()
