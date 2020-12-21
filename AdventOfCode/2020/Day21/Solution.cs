3c53cf4e1ae236c3b169f94cf7bff03274403985
Martijn <karten702@gmail.com>
2020-12-21 13:19:36 +0100

    2020d21

--- AdventOfCode/2020/Day21/Solution.cs
+++ AdventOfCode/2020/Day21/Solution.cs
@@ -9,8 +9,13 @@ namespace AdventOfCode.Y2020.Day21 {
      
     class Solution{
 
-        public static string[] Input =>
-              InputHelper.GetInput(2020, 21).ToArray();
+        public static List<string> Input =>
+              InputHelper.GetInput(2020, 21);
+
+        public static List<string> Test = @"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+                                            trh fvjkl sbzzf mxmxvkd (contains dairy)
+                                            sqjhc fvjkl (contains soy)
+                                            sqjhc mxmxvkd sbzzf (contains fish)".Split(Environment.NewLine).ToList();
 
         public static void Run(){
             Console.WriteLine("Part 1:");
@@ -19,14 +24,75 @@ namespace AdventOfCode.Y2020.Day21 {
             Console.WriteLine("Part 2:");
             Console.WriteLine(Part2());
         }
-        public static bool Part1()
+        public static int Part1()
         {
-            return true;
+            Recipes recipes = new Recipes(Input);
+            HashSet<string> allergies = recipes.PotentialAllergies.Values.SelectMany(a => a).ToHashSet();
+
+            return recipes.IngredientCounts.Where(k => !allergies.Contains(k.Key)).Sum(k => k.Value);
         }
 
-        public static bool Part2()
+        public static string Part2()
         {
-            return true;
+            Recipes recipes = new Recipes(Input);
+            var allergiesDict = recipes.MappedAllergens();
+            return string.Join(',', allergiesDict.OrderBy(k => k.Key).Select(a => a.Value));
+        }
+    }
+
+    class Recipes
+    {
+        public Dictionary<string, int> IngredientCounts = new Dictionary<string, int>();
+        public Dictionary<string, HashSet<string>> PotentialAllergies = new Dictionary<string, HashSet<string>>();
+
+        public Recipes(List<string> input)
+        {
+            foreach (string row in input)
+            {
+                string[] split = row.Split(" (contains ");
+                string[] ingredients = split[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string[] allergies = split[1].Replace(")", "").Split(", ");
+
+                foreach (string ingredient in ingredients)
+                {
+                    if (IngredientCounts.ContainsKey(ingredient))
+                        IngredientCounts[ingredient] += 1;
+                    else
+                        IngredientCounts[ingredient] = 1;
+                }
+
+                foreach (string allergen in allergies)
+                {
+                    if (PotentialAllergies.ContainsKey(allergen))
+                        PotentialAllergies[allergen].IntersectWith(ingredients);
+                    else
+                        PotentialAllergies[allergen] = new HashSet<string>(ingredients);
+                }
+            }
+        }
+
+        public Dictionary<string, string> MappedAllergens()
+        {
+            var allergiesDict = PotentialAllergies;
+
+            while (allergiesDict.Values.Any(x => x.Count != 1))
+            {
+                foreach (string allergen in allergiesDict.Keys)
+                {
+                    HashSet<string> allergenIngredients = allergiesDict[allergen];
+                    if (allergenIngredients.Count != 1)
+                        continue;
+
+                    foreach (string key in allergiesDict.Keys)
+                    {
+                        if (key == allergen)
+                            continue;
+
+                        allergiesDict[key].ExceptWith(allergenIngredients);
+                    }
+                }
+            }
+            return allergiesDict.ToDictionary(d => d.Key, d => d.Value.First());
         }
     }
 }
